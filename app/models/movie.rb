a0f4753ece1dce67153d372f0487d12dd4338bbf
bomoniyi <bomoniyi@chicagobooth.edu>
2021-12-04 20:50:19 +0000

    Added Bootstrap 1 in Application Layout Works

---
+++ app/models/movie.rb
@@ -0,0 +1,28 @@
+# == Schema Information
+#
+# Table name: movies
+#
+#  id          :integer          not null, primary key
+#  description :text
+#  duration    :integer
+#  image       :string
+#  title       :string
+#  year        :integer
+#  created_at  :datetime         not null
+#  updated_at  :datetime         not null
+#  director_id :integer
+#
+class Movie < ApplicationRecord
+  belongs_to :director
+  
+  has_many :characters
+
+  has_many :cast, :through => :characters, :source => :actor
+
+  has_many :bookmarks
+  
+  has_many :bookmarkers, :through => :bookmarks, :source => :user
+
+  validates(:title, { :presence => true })
+
+end
